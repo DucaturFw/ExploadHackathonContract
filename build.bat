ba520a26e193c85fd22122d756d3ef84b6f89e73
zlumer <zlumer@gmail.com>
2018-07-21 17:43:49 +0300

    initial commit

---
+++ build.bat
@@ -0,0 +1,2 @@
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe poker.cs /reference:../pravda/dotnet/src/test/resources/expload.dll 
+pravda compile dotnet --input poker.exe --output poker.pravda
---
+++ poker.cs
@@ -0,0 +1,24 @@
+using System;
+using Com.Expload;
+
+[Program]
+class MyProgram {
+    Mapping<Bytes, int> balances = new Mapping<Bytes, int>();
+
+    public int balanceOf(Bytes tokenOwner) {
+        return balances.getDefault(tokenOwner, 0);
+    }
+
+    public void transfer(Bytes to, int tokens) {
+        if (tokens > 0) {
+            if (balances.getDefault(Info.Sender(), 0) >= tokens) {
+                balances.put(Info.Sender(), balances.getDefault(Info.Sender(), 0) - tokens);
+                balances.put(to, balances.getDefault(to, 0) + tokens);
+            }
+        }
+    }
+}
+
+class MainClass {
+    public static void Main() {}
+}
