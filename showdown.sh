ccf00c8c69a4297b07e2774451dbc7e93bcebd0d
zlumer <zlumer@gmail.com>
2018-07-22 04:50:56 +0300

    showdown test

---
+++ showdown.sh
@@ -0,0 +1,16 @@
+curl -XPOST -H "Content-type: application/json" -d '{
+    "address": "'$1'",
+    "method": "showdown",
+    "args": [
+        {
+            "value": "huila",
+           "tpe": "utf8"
+        },
+        {
+            "value": "050102030405",
+           "tpe": "bytes"
+        }
+    ]
+}' 'http://127.0.0.1:8087/api/program/method'
+
+
